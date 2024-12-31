3e76f7659de568976554c0dc1cef5ca2ae3a2a9c
SwarajZende0310 <zendeswaraj53@gmail.com>
2024-12-31 23:53:22 +0530

    Updated game loop and made to achieve set frame rate

--- LightYears/LightYears/src/framework/Application.cpp
+++ LightYears/LightYears/src/framework/Application.cpp
@@ -1,15 +1,22 @@
+#include<iostream>
 #include "framework/Application.h"
 
 namespace ly
 {
     Application::Application()
-        :mWindow(sf::VideoMode({1024, 1440}), "Light Years")
+        :mWindow(sf::VideoMode({1024, 1440}), "Light Years"),
+        mTargetFrameRate(60.f),
+        mTickClock()
     {
         
     }
 
     void Application::Run()
     {
+        mTickClock.restart();
+        float accumalatedTime = 0.f;
+        float targetDeltaTime = 1.f / mTargetFrameRate;
+
         while(mWindow.isOpen())
         {
             sf::Event windowEvent;
@@ -20,6 +27,25 @@ namespace ly
                     mWindow.close();
                 }
             }
+
+            accumalatedTime += mTickClock.restart().asSeconds();//Calculating the time elapsed before rendering
+            
+            while(accumalatedTime > targetDeltaTime)//Rendering only to achieve det frame rate
+            {
+                accumalatedTime -= targetDeltaTime;
+                Tick(targetDeltaTime);
+                Render();
+            }
         }
+    }
+
+    void Application::Tick(float deltaTime)
+    {
+        // std::cout<<"ticking at framerate: "<< 1.f/deltaTime <<std::endl;
+    }
+
+    void Application::Render()
+    {
+
     }
 }
