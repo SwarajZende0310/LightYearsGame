d727448d48cbc7c471417a48e2121a1ae4092fcd
SwarajZende0310 <zendeswaraj53@gmail.com>
2024-12-31 16:20:00 +0530

    Added Fetch for SFML and linking

--- LightYears/LightYears/src/main.cpp
+++ LightYears/LightYears/src/main.cpp
@@ -1,5 +1,9 @@
 #include<iostream>
+#include<SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Window.hpp>
 
 int main(){
+    sf::RenderWindow renderWindow(sf::VideoMode({800, 600}), "My window");
     std::cout<<"Hello World\n"<<std::endl;
 }
