c93d02156c6ff9e866e89624696d33b0fb5718e7
SwarajZende0310 <zendeswaraj53@gmail.com>
2025-02-01 09:30:33 +0530

    Added HUD Class

---
+++ LightYears/LightYearsEngine/include/widgets/HUD.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include<SFML/Graphics.hpp>
+#include"framework/Object.h"
+
+namespace ly
+{
+    class HUD : public Object
+    {
+        public:
+            virtual void Draw(sf::RenderWindow& windowRef) = 0;
+            void NativeInit(sf::RenderWindow& windowRef);
+            bool HasInit(){ return mAlreadyInit; }
+            virtual bool HandleEvent(const sf::Event& event);
+        
+        protected:    
+            HUD();
+
+        private:
+            virtual void Init(sf::RenderWindow& windowRef);
+            bool mAlreadyInit;
+    };
+}
